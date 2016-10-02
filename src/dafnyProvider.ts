6824ba4716a1ac76e83f8bbdf64e2d139d022036
Jonathan Rosca <roscaj@tcd.ie>
2016-10-02 22:22:16 +0100

    quick save for Dafny.exe launch

--- src/dafnyProvider.ts
+++ src/dafnyProvider.ts
@@ -3,6 +3,7 @@
 import * as path from 'path';
 import * as cp from 'child_process';
 import ChildProcess = cp.ChildProcess;
+
 import * as vscode from 'vscode';
 
 var dafnyPath = '';
--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,14 @@
 import * as vscode from 'vscode';
 
 export class DafnyDiagnostics {
-    public var diagCollection : vscode.;
-
+    public diagCollection: vscode.DiagnosticCollection = null;
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    var inst = new DafnyDiagnostics();
+    context["instance"] = inst;
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
