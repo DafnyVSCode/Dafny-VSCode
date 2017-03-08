fd189297986e06bb2cf8f77cab587917f64737e9
Markus Schaden <mrksschdn@gmail.com>
2017-03-08 15:26:27 +0100

    Added failing tests for test bamboo

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -19,4 +19,36 @@ suite("Extension Tests", () => {
         assert.equal(-1, [1, 2, 3].indexOf(5));
         assert.equal(-1, [1, 2, 3].indexOf(0));
     });
+
+    // Defines a Mocha unit test
+    test("Something 2", () => {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+
+    // Defines a Mocha unit test
+    test("Something 3", () => {
+        assert.equal(true, false);
+    });
+});
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite("Extension Tests 2", () => {
+
+    // Defines a Mocha unit test
+    test("Something 4", () => {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+
+    // Defines a Mocha unit test
+    test("Something 5", () => {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+
+    // Defines a Mocha unit test
+    test("Something 6", () => {
+        assert.equal(true, false);
+    });
 });
