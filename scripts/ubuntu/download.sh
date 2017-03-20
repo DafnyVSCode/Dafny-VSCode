8e5a9711660699656ee565b963f7f870b85d7c3c
Markus Schaden <mrksschdn@gmail.com>
2017-03-20 14:24:20 +0100

    DAF-62 wget fallback

--- scripts/ubuntu/download.sh
+++ scripts/ubuntu/download.sh
@@ -4,13 +4,19 @@ version_url="https://raw.githubusercontent.com/FunctionalCorrectness/Dafny/maste
 
 dafny_ubuntu_url="https://raw.githubusercontent.com/FunctionalCorrectness/Dafny/master/ubuntu.txt"
 
-dafny_url=$(curl $dafny_ubuntu_url)
-echo $dafny_url
-
 cd ~
 mkdir -p ".Dafny"
 cd .Dafny
-curl -L -o "DafnyUbuntu.zip" $dafny_url
+if hash curl 2>/dev/null; then
+    dafny_url=$(curl $dafny_ubuntu_url)
+    echo $dafny_url
+    curl -L -o "DafnyUbuntu.zip" $dafny_url
+else
+    dafny_url="`wget -qO- $dafny_ubuntu_url`"
+    echo $dafny_url
+    wget -O "DafnyUbuntu.zip" $dafny_url
+fi
+
 
 unzip "DafnyUbuntu.zip"
 
