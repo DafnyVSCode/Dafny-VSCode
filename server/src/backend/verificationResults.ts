7d0e8a3cb8083d158f79c86a54a753b71f49d1ed
Markus Schaden <mrksschdn@gmail.com>
2017-04-18 12:58:35 +0200

    DAF-122 Show which postcondition does not hold

--- server/src/backend/verificationResults.ts
+++ server/src/backend/verificationResults.ts
@@ -1,10 +1,10 @@
 "use strict";
 
 import * as vscode from "vscode-languageserver";
-import {IConnection} from "vscode-languageserver";
+import { IConnection } from "vscode-languageserver";
 import { Verification } from "../strings/regexRessources";
 import { EnvironmentConfig, Severity } from "../strings/stringRessources";
-import {VerificationRequest} from "./verificationRequest";
+import { VerificationRequest } from "./verificationRequest";
 
 export enum VerificationStatus {
     Verified = 0,
@@ -23,9 +23,10 @@ export class VerificationResults {
     public latestResults: { [docPathName: string]: VerificationResult } = {};
     private diagCol: vscode.PublishDiagnosticsParams;
 
-    constructor(private connection: IConnection) {    }
+    constructor(private connection: IConnection) { }
 
     public collect(log: string, req: VerificationRequest): VerificationResult {
+        console.log(log);
         const verificationResult: VerificationResult = this.parseVerifierLog(log, req);
         const fileName: string = req.document.uri;
         this.latestResults[fileName] = verificationResult;
@@ -33,7 +34,7 @@ export class VerificationResults {
     }
 
     public addCrashed(req: VerificationRequest): void {
-        if(req != null) {
+        if (req != null) {
             const verificationResult: VerificationResult = new VerificationResult();
             verificationResult.crashed = true;
             const fileName: string = req.document.uri;
@@ -47,6 +48,8 @@ export class VerificationResults {
         const diags: vscode.Diagnostic[] = [];
         let errorCount: number = 0;
         let proofObligations: number = 0;
+        let lastDiagnostic = null;
+        let relatedLocationCounter = 1;
 
         // tslint:disable-next-line:forin
         for (const index in lines) {
@@ -58,7 +61,7 @@ export class VerificationResults {
                 const lineNum: number = parseInt(errors[1], 10) - 1; // 1 based
                 const colNum: number = Math.max(0, parseInt(errors[2], 10) - 1); // 1 based, but 0 can appear in some cases
                 const typeStr: string = errors[3];
-                const msgStr: string = errors[4] !== undefined ? errors[4] + ": " + errors[5] : errors[5];
+                let msgStr: string = errors[4] !== undefined ? errors[4] + ": " + errors[5] : errors[5];
 
                 const start: vscode.Position = vscode.Position.create(lineNum, colNum);
                 const end: vscode.Position = vscode.Position.create(lineNum, Number.MAX_VALUE);
@@ -66,24 +69,54 @@ export class VerificationResults {
 
                 const severity: vscode.DiagnosticSeverity = (typeStr === Severity.Error) ?
                     vscode.DiagnosticSeverity.Error : (typeStr === Severity.Warning) ?
-                    vscode.DiagnosticSeverity.Warning :
-                    vscode.DiagnosticSeverity.Information;
+                        vscode.DiagnosticSeverity.Warning :
+                        vscode.DiagnosticSeverity.Information;
 
                 if (severity === vscode.DiagnosticSeverity.Error) {
                     errorCount++;
                 }
 
-                diags.push(vscode.Diagnostic.create(range, msgStr, severity));
-            } else if(proofObligationLine) {
+                const relatedRange = this.checkForRelatedLocation(lines, index, diags, relatedLocationCounter);
+                if (relatedRange) {
+                    msgStr += " Related location " + relatedLocationCounter + ": Line: " +
+                        (relatedRange.start.line + 1) + ", Col: " + (relatedRange.start.character + 1);
+                    relatedLocationCounter++;
+                }
+
+                lastDiagnostic = vscode.Diagnostic.create(range, msgStr, severity);
+                lastDiagnostic.source = "Dafny VSCode";
+
+                diags.push(lastDiagnostic);
+
+            } else if (proofObligationLine) {
                 proofObligations += parseInt(proofObligationLine[1], 10);
             }
         }
 
-        const publishDiagnosticsParams: vscode.PublishDiagnosticsParams = {uri: req.document.uri, diagnostics: diags};
+        const publishDiagnosticsParams: vscode.PublishDiagnosticsParams = { uri: req.document.uri, diagnostics: diags };
         this.connection.sendDiagnostics(publishDiagnosticsParams);
 
         result.errorCount = errorCount;
         result.proofObligations = proofObligations;
         return result;
+    }
+
+    private checkForRelatedLocation(lines: string[], index: string, diags: vscode.Diagnostic[], relatedLocationCounter: number): vscode.Range {
+        const nextLine: string = lines[(parseInt(index) + 1).toString()];
+        const relatedLocations: RegExpExecArray = Verification.RelatedLocationRegex.exec(nextLine);
+
+        if (relatedLocations) {
+            const lineNum: number = parseInt(relatedLocations[1], 10) - 1; // 1 based
+            const colNum: number = Math.max(0, parseInt(relatedLocations[2], 10) - 1); // 1 based, but 0 can appear in some cases
+            let msgStr: string = relatedLocations[5];
+
+            const start: vscode.Position = vscode.Position.create(lineNum, colNum);
+            const end: vscode.Position = vscode.Position.create(lineNum, Number.MAX_VALUE);
+            const range: vscode.Range = vscode.Range.create(start, end);
+            msgStr = "Related location " + relatedLocationCounter + ". " + msgStr;
+            diags.push(vscode.Diagnostic.create(range, msgStr, vscode.DiagnosticSeverity.Warning, undefined, "Dafny VSCode"));
+
+            return range;
+        }
     }
 }
