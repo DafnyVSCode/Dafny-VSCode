092c1911c0601125ad8ac1102fe8d1bd79f8c7a9
ValisStigma <rafael_krucker@hotmail.com>
2017-05-15 09:19:29 +0200

    fixed typo

--- client/src/dafnyProvider.ts
+++ client/src/dafnyProvider.ts
@@ -19,7 +19,7 @@ export class DafnyClientProvider {
     private counterModelProvider: CounterModelProvider;
     private context: Context;
     private dotGraphProvider: DotGraphProvider;
-    private previewUri = vscode.Uri.parse('dafny-preview:State Visualization');;
+    private previewUri = vscode.Uri.parse("dafny-preview:State Visualization");
 
     constructor(public vsCodeContext: vscode.ExtensionContext, public languageServer: LanguageClient) {
         const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(EnvironmentConfig.Dafny);
@@ -70,8 +70,6 @@ export class DafnyClientProvider {
                 //Log.error("HTML Preview error: " + reason);
             });
         });
-
-
     }
 
     public dispose(): void {
